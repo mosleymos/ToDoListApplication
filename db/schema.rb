f57dc1c1dcb1da445594a4050c2d8987f142d9bd
Mori <keitamori@gmail.com>
2015-09-24 18:27:56 +0200

    Mise a jour du schema migrations

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,7 +13,7 @@
 
 ActiveRecord::Schema.define(version: 20131030132115) do
 
-  create_table "todos", force: true do |t|
+  create_table "todos", force: :cascade do |t|
     t.string  "text"
     t.integer "status"
   end
